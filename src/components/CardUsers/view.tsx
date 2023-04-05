c1ba3800b6eb4b46ab54b55c69077744626fef9f
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-04-05 15:26:30 -0300

    create component card user

---
+++ src/components/CardUsers/view.tsx
@@ -0,0 +1,7 @@
+export function CardUsersView() {
+  return (
+    <div className="flex flex-row justify-between bg-cyan-900 h-32 w-1/3 rounded-xl m-8">
+      <div className="w-32 h-full bg-gray-700 rounded-l-xl border-none outline-none"></div>
+    </div>
+  );
+}
