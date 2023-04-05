a203bc8423d876409dfdf28f93964062d2f28ad0
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-04-05 15:30:13 -0300

    add component call

--- src/screens/ListUsers/view.tsx
+++ src/screens/ListUsers/view.tsx
@@ -1,3 +1,11 @@
+import { CardUser, Title } from "../../components";
+
 export function ListUsersView() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-900">
+      <Title> List users </Title>
+
+      <CardUser></CardUser>
+    </div>
+  );
 }
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,2 @@
 export { LoginView as Login } from "./Login/view";
+export { ListUsersView as ListUsers } from "./ListUsers/view";
