7e570117e90c4a7f6ac6650c93c44037a4e23c17
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-04-05 15:27:42 -0300

    refactory index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-export { TitleView as Title } from "./Title/view";
-export { SpanView as Span } from "./Span/view";
-export * from "./Input/view";
 export { ButtonView as Button } from "./Buttom/view";
+export { CardUsersView as CardUser } from "./CardUsers/view";
 export { ErrorView as Error } from "./Error/view";
+export * from "./Input/view";
+export { SpanView as Span } from "./Span/view";
+export { TitleView as Title } from "./Title/view";
